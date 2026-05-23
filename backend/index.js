aa46612b84e5779b3a0ca78ae6340874d71e43d3
Saudarsh Pal <saudarsh29@gmail.com>
2026-05-23 12:50:55 +0530

    feat: update cors origin

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,13 @@ const app = express()
 connectToDB();
 
 
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:7070",
+    credentials : true,
+    methods : ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
+
 app.use(express.json())
 
 app.get('/',(req,res)=>{
