f0ca9940d26fc7e6f5dfbf7510ff7d47f93d465e
Saudarsh Pal <saudarsh29@gmail.com>
2026-05-25 09:46:05 +0530

    feat: update post model
    - only single image approved per post
    - add tags property ( 5 each post) to post model.
    - add db index on author and community
    - remove comment property from post model

--- backend/models/postModel.js
+++ backend/models/postModel.js
@@ -1,33 +1,55 @@
 import mongoose from "mongoose";
+import { maxLength } from "zod";
 
 
-const postSchema  = new mongoose.Schema({
-    title :{
-        type : String,
-        required : true
-    },
-    content :{
-        type : String ,
-        required : true
-    },
-    images : {
-        type :[{
-            url : String,
-            public_id : String
-        }
-        ],
-        default :[]
-    },
-    author :{
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'User',
-        required : true
-    },
-    community : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'Community'
-    },
-    votes : {
+const postSchema  = new mongoose.Schema(
+    {
+        title :{
+            type : String,
+            required : true,
+            maxLength: 200,
+            trim: true
+        },
+        content :{
+            type : String ,
+            required : true,
+            maxLength: 100000
+        },
+        images : {
+            exists: {
+                type: Boolean,
+                default: false
+            },
+            url: {
+                type: String,
+                default: null
+            },
+            public_id: {
+                type: String,
+                default: null
+            }
+        },
+        tags: {
+            type: [String],
+            default: [],
+            validate: {
+                validator: (arr) => arr.length <= 5,
+                message: "A post have at most 5 tags"
+            },
+            index: true
+        },
+        author :{
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'User',
+            required : true,
+            index: true
+        },
+        community : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'Community',
+            default: null,
+            index: true,
+        },
         upvotes : {
             type : Number,
             default : 0
@@ -35,32 +57,28 @@ const postSchema  = new mongoose.Schema({
         downvotes : {
             type : Number,
             default : 0
-        }
-    },
-    comments : [
-        {
-            author : {
-                type : mongoose.Schema.Types.ObjectId,
-                ref : 'User',
-                required : true
-            },
-            content : {
-                type : String,
-                required : true,
-            },
-            votes : {
-                upvotes : {
-                    type : Number,
-                    default : 0
+        },
+        voters: [
+            {
+                user: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: 'User'
                 },
-                downvotes : {
-                    type : Number,
-                    default : 0
+                voteTyepe: {
+                    type: String,
+                    enum: ["up","down"]
                 }
             }
-        }
-    ]   
-})
+        ]
+    },
+    {
+        timestamps:true
+    }
+)
+
+postSchema.index({ community: 1, createdAt: -1 })
+ 
+postSchema.index({ author: 1, createdAt: -1 })
 
 
 const Post = mongoose.model('Post',postSchema)
