f4a4a5b60a41ba21753e1affaef47b4a4b652b60
Saudarsh Pal <saudarsh29@gmail.com>
2026-05-25 09:41:28 +0530

    feat: update user model
    - remove gender and banner property from user profile
    - add public_id to profile avatar for manipulation
    - add bio, followers and following count property
    - add timestamps to user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,64 +1,81 @@
 import mongoose from "mongoose";
+import { maxLength } from "zod";
 
-const userSchema = new mongoose.Schema({
-    username : {
-        type : String,
-        unique : true,
-        required : true,
-        minLength : 5,
-        maxLength : 20,
-    },
-    email : {
-        type : String,
-        unique : true,
-        required : true,       
-    },
-    password : {
-        type : String,
-        required : true,
-        minLength : 8,
-    },
-    followers : [
-        {
-            type : mongoose.Schema.Types.ObjectId,
-            ref : 'User'
-        }
-    ],
-    following : [
-        {
-            type : mongoose.Schema.Types.ObjectId,
-            ref : 'User'
-        }
-    ],
-    profile : {
-        gender : {
-           type : String,
-           default : null,
-           enum : ['Male',"Female"]
+const userSchema = new mongoose.Schema(
+    {
+        username : {
+            type : String,
+            unique : true,
+            required : true,
+            minLength : 5,
+            maxLength : 20,
+            trim: true
         },
-        avatar : {
-            exists : {
-                type : Boolean,
-                default : false
-            },
-            url : {
-                type : String,
-                default : null
+        email : {
+            type : String,
+            unique : true,
+            required : true,
+            trim: true,     
+        },
+        password : {
+            type : String,
+            required : true,
+            minLength : 8,
+        },
+
+        bio : {
+            type: String,
+            default: null,
+            maxLength: 180
+        },
+        followers: [
+            { 
+                type : mongoose.Schema.Types.ObjectId,
+                ref : 'User' 
+            }
+        ],
+        following: [
+            { 
+                type : mongoose.Schema.Types.ObjectId,
+                ref : 'User' 
+            }
+        ],
+        followerCount: {
+            type: Number,
+            default: 0 
+        },
+        followingCount: { 
+            type: Number,
+            default: 0
+        },
+        profile : {
+            avatar : { 
+                exists: {
+                    type : Boolean,
+                    default : false
+                },
+                url : { 
+                    type : String,
+                    default : null
+                },
+                public_id: { 
+                    type: String,
+                    default: null
+                }
             }
         },
-        banner:{
-            exists: {
-                type : Boolean,
-                default : false
-            },
-            url : {
-                type : String,
-                default : null
+
+        subscribedCommunitties: [
+            { 
+                type: mongoose.Schema.Types.ObjectId, 
+                ref: "Community"
             }
-        }
+        ],
+    },
+    {
+        timestamps: true
     }
-})
+)
 
 const User = mongoose.model('User',userSchema)
-
 export default User
