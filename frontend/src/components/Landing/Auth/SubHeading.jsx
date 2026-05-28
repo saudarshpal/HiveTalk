4f7c514e309ff417edd9ef040c663014ab65e2d6
Saudarsh Pal <saudarsh29@gmail.com>
2026-05-28 23:49:50 +0530

    chore: remove frontend completely

--- frontend/src/components/Landing/Auth/SubHeading.jsx
+++
@@ -1,9 +0,0 @@
-const SubHeading = ({label}) => {
-  return ( 
-        <div className="text-md text-white font-medium  py-2 px-4 text-center">
-            {label}
-        </div>
-  )
-}
-
-export default SubHeading
